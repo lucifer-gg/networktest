cb4c34b7c41e348ae660310ecd00b464582f4a6c
lucifer-gg <1140737321@qq.com>
2021-12-19 16:15:52 +0800

    init

---
+++ src/main/java/com/nju/networktest/NetworkTestApplication.java
@@ -0,0 +1,13 @@
+package com.nju.networktest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NetworkTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NetworkTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nju/networktest/NetworkTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nju.networktest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NetworkTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
