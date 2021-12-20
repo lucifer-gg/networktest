0d0d5b0a9d9bf9e7d5d08855d3dab21a22381063
lucifer-gg <1140737321@qq.com>
2021-12-20 13:08:35 +0800

    初始框架

---
+++ src/main/java/com/nju/networktest/service/Impl/ScriptFileServiceImpl.java
@@ -0,0 +1,27 @@
+package com.nju.networktest.service.Impl;
+
+import com.nju.networktest.service.ScriptFileService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ScriptFileServiceImpl implements ScriptFileService {
+    @Override
+    public boolean addScriptFile() {
+        return false;
+    }
+
+    @Override
+    public boolean updateScriptFile() {
+        return false;
+    }
+
+    @Override
+    public boolean deleteScriptFile() {
+        return false;
+    }
+
+    @Override
+    public String getScriptFileContent() {
+        return null;
+    }
+}
---
+++ src/main/java/com/nju/networktest/service/ScriptFileService.java
@@ -0,0 +1,11 @@
+package com.nju.networktest.service;
+
+public interface ScriptFileService {
+    public boolean addScriptFile();
+
+    public boolean updateScriptFile();
+
+    public boolean deleteScriptFile();
+
+    public String getScriptFileContent();
+}
